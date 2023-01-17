fda94d1f85eaafe48167b55ddc0b18d7f956c5db
ashish <ashishroy7652@gmail.com>
2023-01-17 14:38:59 +0530

    server socket for receiving message

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const express = require('express') ; 
+const express = require("express");
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io') ; 
-const cors = require("cors") ; 
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-
-app.use(cors()) ; 
+app.use(cors());
 
 var server = http.createServer(app);
-const io = new Server(server , {
-    cors: {
-        origin: '*',
-      }
-}) ; 
-
-
-
-io.on('connection', function(socket){
-  console.log(`User connected : ${socket.id}`) ; 
-
-  socket.on("send_message" , (data)=>{
-        console.log(data) ; 
-        io.sockets.emit("receive_message", data );
-      })
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
+io.on("connection", function (socket) {
+  console.log(`User connected : ${socket.id}`);
 
-server.listen(5001 , ()=>{
-    console.log("Server is Running") ; 
-})
+  socket.on("send_message", (data) => {
+    console.log(data);
+    io.sockets.emit("receive_message", data);
+  });
 
+  socket.on("send_not_done", (data) => {
+    console.log(data);
+    io.sockets.emit("receive_status", data);
+  });
 
+  socket.on("send_done", (data) => {
+    console.log(data);
+    io.sockets.emit("receive_status", data);
+  });
+
+  socket.on("send_no_status", (data) => {
+    console.log(data);
+    io.sockets.emit("receive_status", data);
+  });
+
+  socket.on("delete" , data=>{
+    console.log(data) ; 
+    io.sockets.emit("receive_delete" , data) ; 
+  })
+});
+
+server.listen(5000, () => {
+  console.log("Server is Running");
+});
